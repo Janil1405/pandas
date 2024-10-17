d8ecba3bff481ad0107971fbdcef5e2202c3a1e2
Janilson Alves da Silva <99848376+Janil1405@users.noreply.github.com>
2024-10-16 21:14:04 -0300

    oi
    oi

--- app.py
+++ app.py
@@ -18,10 +18,43 @@ st.subheader("Isso aqui é um sub-cabeçalho", divider=True)
 st.text("Isso aqui é um texto.")
 
 
-df=pd.DataFrame(np.random.randn(50,20)), columns=("col %d" % i for i in range(20))
+df = pd.DataFrame(np.random.randn(50,20), columns=("col %d" % i for i in range(20)))
 st.dataframe(df)
 
 selecao1 = st.checkbox ("Marcado")
 
-if selecao1
+if selecao1:
     st.write(selecao1)
+    
+    
+#Elementos de Seleção
+
+#Checkbox
+selecao1= st.checkbox("Marcado")
+
+if selecao1:
+    st.write(selecao1)
+    
+# Cor
+
+cor = st.color_picker("Selecione a cor, #00f900")
+st.write("Cor selecionada", cor)
+
+
+#Multselect
+
+opcoes = st.multiselect(
+    "Selecione as opções",
+    ["Green", "Yellow", "Red", "Blue"]
+)
+st.write("Sua seleção", opcoes)
+  
+  
+
+    
+    
+    
+    
+
+
+    
